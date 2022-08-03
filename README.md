e420482589af6aa656604fabbe41cbc846556d1e
SmolAlli <allikat2706@gmail.com>
2022-08-03 22:39:13 +1000

    Add new plugin TwitchInVC

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ A collection of my plugins.
 
 Current plugins:
 
-- FixEsc - Temporarily fixes Discord's issue where `esc` no longer makes the chat jump down to the most recent message. It's a simple solution, but it works.
+-   FixEsc - Temporarily fixes Discord's issue where `esc` no longer makes the chat jump down to the most recent message. It's a simple solution, but it works.
+
+-   TwitchInVC (tentative) - Shows an indication that someone in a VC is also live on Twitch.
---
+++ TwitchInVC/TwitchInVC.js
@@ -0,0 +1,170 @@
+/**
+ * @name TwitchInVC
+ * @version 0.0.1
+ * @author SmolAlli
+ *
+ */
+
+module.exports = (() => {
+    const config = {
+        info: {
+            name: "TwitchInVC",
+            author: {
+                name: "SmolAlli",
+            },
+            version: "0.0.1",
+            description:
+                "Changes the name colours of people in VC if they are live on Twitch",
+        },
+        main: "index.js",
+    };
+
+    return !global.ZeresPluginLibrary
+        ? class {
+              constructor() {
+                  this._config = config;
+              }
+              getName() {
+                  return config.info.name;
+              }
+              getAuthor() {
+                  return config.info.authors.map((a) => a.name).join(", ");
+              }
+              getDescription() {
+                  return config.info.description;
+              }
+              getVersion() {
+                  return config.info.version;
+              }
+              load() {
+                  BdApi.showConfirmationModal(
+                      "Library Missing",
+                      `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+                      {
+                          confirmText: "Download Now",
+                          cancelText: "Cancel",
+                          onConfirm: () => {
+                              require("request").get(
+                                  "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+                                  async (error, response, body) => {
+                                      if (error)
+                                          return require("electron").shell.openExternal(
+                                              "https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+                                          );
+                                      await new Promise((r) =>
+                                          require("fs").writeFile(
+                                              require("path").join(
+                                                  BdApi.Plugins.folder,
+                                                  "0PluginLibrary.plugin.js"
+                                              ),
+                                              body,
+                                              r
+                                          )
+                                      );
+                                  }
+                              );
+                          },
+                      }
+                  );
+              }
+              start() {}
+              stop() {}
+          }
+        : (([Plugin, Api]) => {
+              const plugin = (Plugin, Api) => {
+                  const TwitchColour = "#9146ff";
+                  const { WebpackModules, DiscordModules, Patcher, Utilities } =
+                      Api;
+
+                  const GuildMemberStore = DiscordModules.GuildMemberStore;
+                  const SelectedGuildStore = DiscordModules.SelectedGuildStore;
+                  const VoiceUser =
+                      WebpackModules.getByDisplayName("VoiceUser");
+
+                  return class TwitchInVC extends Plugin {
+                      onStart() {
+                          Utilities.suppressErrors(
+                              this.patchVoiceUsers.bind(this),
+                              "voice users patch"
+                          )();
+                          this.promises = {
+                              state: { cancelled: false },
+                              cancel() {
+                                  this.state.cancelled = true;
+                              },
+                          };
+                      }
+
+                      onStop() {
+                          Patcher.unpatchAll();
+                          this.promises.cancel();
+                          if (this.unpatchAccountDetails) {
+                              this.unpatchAccountDetails();
+                              delete this.unpatchAccountDetails;
+                          }
+                      }
+
+                      getSettingsPanel() {
+                          return this.buildSettingsPanel().getElement();
+                      }
+
+                      getMember(userId, guild = "") {
+                          const guildId =
+                              guild || SelectedGuildStore.getGuildId();
+                          if (!guildId) return null;
+                          const member = GuildMemberStore.getMember(
+                              guildId,
+                              userId
+                          );
+                          if (!guildId) return null;
+                          return member;
+                      }
+
+                      patchVoiceUsers() {
+                          Patcher.after(
+                              VoiceUser.prototype,
+                              "renderName",
+                              (thisObject, _, returnValue) => {
+                                  console.log(thisObject);
+                                  //   console.log(_);
+                                  //   console.log(returnValue);
+                                  if (!returnValue || !returnValue.props)
+                                      return;
+
+                                  const GetStatus = BdApi.findModuleByProps(
+                                      "getStatus",
+                                      "getState"
+                                  );
+
+                                  const UserActivities =
+                                      GetStatus.getActivities(
+                                          thisObject.props.user.id
+                                      );
+                                  let isOnTwitch = false;
+                                  UserActivities.forEach((elem) => {
+                                      if (elem.name === "Twitch") {
+                                          isOnTwitch = true;
+                                      }
+                                  });
+
+                                  if (!isOnTwitch) return;
+                                  returnValue.props.style = {
+                                      color: TwitchColour,
+                                      backfaceVisibility: "hidden",
+                                  };
+                                  returnValue.ref = (element) => {
+                                      if (!element) return;
+                                      element.style.setProperty(
+                                          "color",
+                                          TwitchColour,
+                                          "important"
+                                      );
+                                  };
+                              }
+                          );
+                      }
+                  };
+              };
+              return plugin(Plugin, Api);
+          })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
